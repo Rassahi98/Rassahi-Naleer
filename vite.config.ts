408d69d688a8df47c193889bc370093e8d84df8f
Rassahi98 <rassahinaleer@gmail.com>
2025-08-15 23:23:18 +0530

    Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // important for Vercel static deployment
+  base: "./",
   server: {
     host: "::",
     port: 8080,
@@ -20,6 +19,6 @@ export default defineConfig(({ mode }) => ({
     },
   },
   build: {
-    outDir: "dist", // default Vite output directory
+    outDir: "dist",
   },
 }));
